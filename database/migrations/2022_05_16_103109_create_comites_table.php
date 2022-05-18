f4c0461ed1f209b5b3898759206687881f4c967e
eric <erichogedez2@gmail.com>
2022-05-18 10:00:12 +0200

    ericComite

--- database/migrations/2022_05_16_103109_create_comites_table.php
+++ database/migrations/2022_05_16_103109_create_comites_table.php
@@ -20,7 +20,7 @@ return new class extends Migration
             $table->string('lastnamePresident');
             $table->string('adress');
             $table->string('email');
-            $table->integer('phone')->nullable();
+            $table->number('phone')->nullable();
             $table->string('facebookLink')->nullable();
             $table->string('webSite')->nullable();
             $table->string('description');
